fb4f70646103e201c773964eaa79775f2d503d75
MrKube <bertrand.kuzbinski@gmail.com>
2025-03-21 13:51:15 +0100

    Refactor LDAP connection and user validation functions for improved readability and efficiency

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import time
 import logging
 import ldap3
-from ldap3 import Server, Connection, ALL, NTLM
+from ldap3 import Server, Connection, ALL, NTLM, MODIFY_REPLACE
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
@@ -23,11 +23,9 @@ logging.basicConfig(
 )
 
 def get_ldap_connection():
-    """Établit une connexion LDAP sécurisée en utilisant les informations d'identification fournies."""
+    """Établit une connexion LDAP sécurisée."""
     server = Server(config['ldap']['server'], use_ssl=True, get_info=ALL)
-
     logging.info(f"Connecting to LDAP server: {config['ldap']['server']}")
-    logging.info(f"Using domain: {config['ldap']['domain']}")
 
     try:
         conn = Connection(server,
@@ -46,29 +44,20 @@ def get_ldap_connection():
 def is_user_admin(conn, user_dn):
     """Vérifie si un utilisateur est un administrateur."""
     conn.search(config['search_base'], f'(member={user_dn})', attributes=['cn'])
-    if conn.entries:
-        for entry in conn.entries:
-            if 'admin' in entry.cn.lower():
-                return True
-    return False
+    return any('admin' in entry.cn.lower() for entry in conn.entries)
 
 def is_user_in_vip_group(conn, user_dn):
     """Vérifie si un utilisateur appartient au groupe VIP."""
     conn.search(config['search_base'], f'(member={user_dn})', attributes=['cn'])
-    if conn.entries:
-        for entry in conn.entries:
-            if entry.dn.lower() == config['ldap']['vipGroup'].lower():
-                return True
-    return False
+    return any(entry.dn.lower() == config['ldap']['vipGroup'].lower() for entry in conn.entries)
 
 def find_user_dn(conn, sam_account_name, search_base):
     """Recherche le DN d'un utilisateur en fonction de son samAccountName."""
     conn.search(search_base, f'(samAccountName={sam_account_name})', attributes=['distinguishedName'])
     if conn.entries:
         return conn.entries[0].distinguishedName.value
-    else:
-        logging.error(f"User {sam_account_name} not found in the directory.")
-        return None
+    logging.error(f"User {sam_account_name} not found in the directory.")
+    return None
 
 def process_json_file(file_path, total_files):
     """Traite un fichier JSON pour réinitialiser le mot de passe."""
@@ -98,7 +87,6 @@ def process_json_file(file_path, total_files):
     if not conn:
         return
 
-    # Rechercher le DN de l'utilisateur
     user_dn = find_user_dn(conn, user_samAccountName, config['search_base'])
     if not user_dn:
         logging.error(f"Failed to locate user {user_samAccountName} in the directory.")
@@ -113,9 +101,9 @@ def process_json_file(file_path, total_files):
     if is_user_in_vip_group(conn, user_dn):
         logging.info(f"User {user_samAccountName} is in VIP group. Skipping password reset.")
         return
-    
 
     new_password = generate_password(user_samAccountName)
+    logging.info(f"User {user_samAccountName} Reset New Password : {new_password}")
 
     try:
         if not config['debug_mode']:
@@ -126,7 +114,7 @@ def process_json_file(file_path, total_files):
                 send_notification_email(user_samAccountName, new_password, sponsor_email)
                 send_notification_email(user_samAccountName, new_password, config['itServiceEmail'])
                 move_processed_file(file_path)
-                conn.modify(user_dn, {'pwdLastSet': (ldap3.MODIFY_REPLACE, [0])})
+                conn.modify(user_dn, {'pwdLastSet': (MODIFY_REPLACE, [0])})
             else:
                 logging.error(f"Failed to reset password for {user_samAccountName}: {conn.result['description']} (Result code: {conn.result['result']})")
 
@@ -139,91 +127,24 @@ def process_json_file(file_path, total_files):
     except Exception as e:
         logging.error(f"Unexpected error: {e}")
 
-    """Traite un fichier JSON pour réinitialiser le mot de passe."""
-    try:
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-    except Exception as e:
-        logging.error(f"Error reading JSON file {file_path}: {e}")
-        return
-
-    file_date = datetime.fromtimestamp(os.path.getmtime(file_path)).strftime('%Y-%m-%d %H:%M:%S')
-    logging.info(f"Processing file: {file_path}, File date: {file_date}, Total files to process: {total_files}")
-
-    # Utilisez les clés correctes du fichier JSON
-    sponsor_samAccountName = data.get('samAccountName', 'Unknown Sponsor')
-    user_samAccountName = data.get('user_samAccountName', 'Unknown User')
-    sponsor_email = data.get('email', 'Unknown Email')
-
-    logging.info(f"Sponsor: {sponsor_email} ({sponsor_samAccountName}), Account to reset: {user_samAccountName}")
-
-    # Vérifier si le demandeur essaie de réinitialiser son propre mot de passe
-    if sponsor_samAccountName.lower() == user_samAccountName.lower():
-        logging.error(f"Self-reset attempt detected: {sponsor_samAccountName} trying to reset their own password. Request ignored.")
-        send_rejection_email(sponsor_email, user_samAccountName)
-        move_processed_file(file_path)
-        return
-
-    conn = get_ldap_connection()
-    if not conn:
-        return
-
-    user_dn = f"CN={user_samAccountName},{config['search_base']}"
-
-    # Vérifier si l'utilisateur est un administrateur ou dans le groupe VIP
-    if is_user_admin(conn, user_dn):
-        logging.info(f"User {user_samAccountName} is an admin. Skipping password reset.")
-        return
-
-    if is_user_in_vip_group(conn, user_dn):
-        logging.info(f"User {user_samAccountName} is in VIP group. Skipping password reset.")
-        return
-
-    new_password = generate_password(user_samAccountName)
-
-    if config['debug_mode']:
-        logging.info(f"DEBUG MODE: Simulating password reset for {user_samAccountName}. New password would be: {new_password}")
-        # Envoyer les deux emails séparément
-        send_notification_email(user_samAccountName, new_password, sponsor_email)
-        send_notification_email(user_samAccountName, new_password, config['itServiceEmail'])
-        move_processed_file(file_path)
-    else:
-        try:
-            conn.extend.microsoft.modify_password(user_dn, new_password)
-
-            if conn.result['result'] == 0:
-                logging.info(f"Password successfully reset for {user_samAccountName}")
-                # Envoyer les deux emails séparément
-                send_notification_email(user_samAccountName, new_password, sponsor_email)
-                send_notification_email(user_samAccountName, new_password, config['itServiceEmail'])
-                move_processed_file(file_path)
-                conn.modify(user_dn, {'pwdLastSet': (ldap3.MODIFY_REPLACE, [0])})
-            else:
-                logging.error(f"Failed to reset password for {user_samAccountName}: {conn.result['description']}")
-        except Exception as e:
-            logging.error(f"Error resetting password: {e}")
-
 def generate_password(username):
-    """Génère un mot de passe sécurisé selon les spécifications."""
+    """Génère un mot de passe sécurisé."""
     length = 12
     all_characters = string.ascii_letters + string.digits + "!@#$%^&*()-_=+[]{}|;:,.<>?"
 
     while True:
         password = ''.join(secrets.choice(all_characters) for _ in range(length))
-
-        # Vérifier les caractéristiques du mot de passe
-        if (any(c.islower() for c in password) +
-            any(c.isupper() for c in password) +
-            any(c.isdigit() for c in password) +
-            any(c in "!@#$%^&*()-_=+[]{}|;:,.<>?" for c in password) >= 3 and
+        if (any(c.islower() for c in password) and
+            any(c.isupper() for c in password) and
+            any(c.isdigit() for c in password) and
+            any(c in "!@#$%^&*()-_=+[]{}|;:,.<>?" for c in password) and
             username.lower() not in password.lower()):
             return password
 
 def send_notification_email(username, new_password, recipient_email):
     """Envoie un e-mail de notification HTML après la réinitialisation du mot de passe."""
-    # Échapper les caractères spéciaux HTML dans le mot de passe
     escaped_password = new_password.replace('<', '&lt;').replace('>', '&gt;')
-    
+
     html_content = f"""
     <html>
     <head>
@@ -269,7 +190,6 @@ def send_notification_email(username, new_password, recipient_email):
     msg['From'] = config['smtp']['fromAddress']
     msg['To'] = recipient_email
 
-    # Ajouter une version texte pour les clients qui ne supportent pas HTML
     text_part = MIMEText(f"The password for {username} has been reset to: {new_password}", 'plain')
     html_part = MIMEText(html_content, 'html')
 
@@ -328,7 +248,7 @@ def main():
             file_path = os.path.join(json_dir, filename)
             process_json_file(file_path, total_files)
 
-        time.sleep(config['scan_interval'])  # Attendre selon l'intervalle défini dans le fichier de configuration
+        time.sleep(config['scan_interval'])
 
 if __name__ == '__main__':
     main()
